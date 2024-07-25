0e531e285e22eb8a4782fb31b82e10c9c856a22d
KIA <mojtabahosseinikia@gmail.com>
2024-07-25 06:59:56 +0330

    main

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,11 +5,10 @@ import './custom.css'
 //export default DefaultTheme
 
 import MyLayout from './MyLayout.vue'
-import MyLayoutMobile from './MyLayoutMobile.vue'
 
 export default {
   extends: DefaultTheme,
   // override the Layout with a wrapper component that
   // injects the slots
-  Layout: MyLayout, MyLayoutMobile
+  Layout: MyLayout
 }
